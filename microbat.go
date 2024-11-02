1090a70bcbaa0f797a1c1df068e6e1a0c607bb3d
Nicholas Terrell <nicholas@terrell.com.au>
2024-11-03 08:00:45 +0900

    add job batch

--- microbat.go
+++ microbat.go
@@ -2,6 +2,7 @@ package microbat
 
 import (
 	"context"
+	"sync"
 	"time"
 )
 
@@ -9,7 +10,9 @@ type Job interface {
 	ID() string
 }
 
-type JobResult interface{}
+type JobResult interface {
+	ID() string
+}
 
 type BatchProcessor func(ctx context.Context, job ...Job) ([]JobResult, error)
 
@@ -25,12 +28,45 @@ type Client struct {
 	processor BatchProcessor
 	batchSize int
 	waitTime  time.Duration
+	jobLock   sync.Mutex
+	jobs      []Job
 }
 
 func (c *Client) Close() error {
 	return nil
 }
 
-func (c *Client) AddJob(job ...Job) error {
+func (c *Client) AddJob(jobs ...Job) error {
+	c.jobLock.Lock()
+	defer c.jobLock.Unlock()
+
+	c.jobs = append(c.jobs, jobs...)
 	return nil
+}
+
+func (c *Client) JobCount() int {
+	c.jobLock.Lock()
+	defer c.jobLock.Unlock()
+
+	return len(c.jobs)
+}
+
+func (c *Client) GetNextBatch() []Job {
+	c.jobLock.Lock()
+	defer c.jobLock.Unlock()
+
+	if len(c.jobs) == 0 {
+		return []Job{}
+	}
+
+	if len(c.jobs) <= c.batchSize {
+		jobs := c.jobs
+		c.jobs = []Job{}
+		return jobs
+	}
+
+	jobs := c.jobs[:c.batchSize]
+	c.jobs = c.jobs[c.batchSize:]
+
+	return jobs
 }
--- microbat_test.go
+++ microbat_test.go
@@ -2,9 +2,14 @@ package microbat
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 )
+
+func mockBatchProcessor(ctx context.Context, job ...Job) ([]JobResult, error) {
+	return nil, nil
+}
 
 func TestMicroBat(t *testing.T) {
 
@@ -14,5 +19,29 @@ func TestMicroBat(t *testing.T) {
 
 	client := New(batchProcessor, 2, time.Second*1)
 	defer client.Close()
+
+}
+
+type MockJob struct {
+	id string
+}
+
+func (m *MockJob) ID() string {
+	return m.id
+}
+
+func TestNextBatch(t *testing.T) {
+	client := New(mockBatchProcessor, 2, time.Second*5)
+
+	for i := 0; i < 10; i++ {
+		client.AddJob(&MockJob{id: fmt.Sprintf("%d", i)})
+	}
+
+	var batchCount int
+	for client.JobCount() > 0 {
+		jobs := client.GetNextBatch()
+		fmt.Printf("got %d jobs\n", len(jobs))
+		batchCount++
+	}
 
 }
